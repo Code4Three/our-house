7b5d5b87c0042ea68053f181a25a26c1865b1f2e
Sean Coleman <seancoleman@gmail.com>
2019-01-08 09:48:38 +1100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,8 +30,8 @@ module.exports = {
         name: 'Our House',
         short_name: 'Us',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#0D646A',
+        theme_color: '#0D646A',
         display: 'minimal-ui',
         //icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
