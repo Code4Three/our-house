48cb8ee5ba1620af0b68fcf49cfadd77e8b75cc0
Sean Coleman <seancoleman@gmail.com>
2019-01-12 08:21:12 +1100

    refactored dayCard.js to use css module

--- src/components/day-card.js
+++ src/components/day-card.js
@@ -1,63 +1,15 @@
 import React, { Component } from 'react';
+import styles from './day-card.module.css';
 
 class DayCard extends Component {
   render() {
     return (
-      <div
-        style={{
-          display: 'flex',
-          flexGrow: '50',
-          minHeight: '4rem',
-          width: 'auto',
-          margin: '0.8rem 1.2rem',
-          border: '1px solid var(--dmed-grey)',
-          borderRadius: '0.2rem',
-          boxShadow: '0.5rem 0.5rem 0.8rem var(--shadow)',
-          backgroundColor: 'var(--dark-grey)',
-        }}
-      >
-        <button
-          onClick={this.props.decAction}
-          style={{
-            width: '2rem',
-            margin: '0',
-            border: '0',
-            borderRadius: '0.2rem',
-            padding: '0',
-            backgroundColor: 'var(--dark-grey)',
-            color: '#ffffff',
-          }}
-        >
+      <div className={styles.container}>
+        <button className={styles.buttons} onClick={this.props.decAction}>
           {`<`}
         </button>
-        <h2
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            flexGrow: 1,
-            width: 'auto',
-            margin: '0',
-            border: '0',
-            padding: '0',
-            backgroundColor: 'var(--dark-grey)',
-            color: '#ffffff',
-          }}
-        >
-          {this.props.day}
-        </h2>
-        <button
-          onClick={this.props.incAction}
-          style={{
-            width: '2rem',
-            margin: '0',
-            border: '0',
-            borderRadius: '0.2rem',
-            padding: '0',
-            backgroundColor: 'var(--dark-grey)',
-            color: '#ffffff',
-          }}
-        >
+        <h2 className={styles.title}>{this.props.day}</h2>
+        <button className={styles.buttons} onClick={this.props.incAction}>
           {`>`}
         </button>
       </div>
