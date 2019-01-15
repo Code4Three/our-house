b46a25f8ed15d85f534bba4e989cd103cbc3a9ec
Sean Coleman <seancoleman@gmail.com>
2019-01-15 21:37:34 +1100

    Home page draft complete, needs tuning

--- src/components/home-card.js
+++ src/components/home-card.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
-import styles from './h2-card.module.css';
+import styles from './home-card.module.css';
 
-class DayCard extends Component {
+class HomeCard extends Component {
   render() {
     return (
       <div className={styles.container}>
+        <h5 className={styles.tag}>{this.props.tag}</h5>
         <h2 className={styles.title}>{this.props.title}</h2>
       </div>
     );
   }
 }
 
-export default DayCard;
+export default HomeCard;
