6ca7aa412a22fe21441b1059dd6cc24b6640b124
Sean Coleman <seancoleman@gmail.com>
2019-02-05 20:41:42 +1100

    slight modification to bg grey, aligned job names to left, added drop shadow to header (green)

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
+import styles from './header.module.css';
 
 import './layout.css';
 
 const Header = ({ siteTitle }) => (
-  <header
-    className="header"
-    style={{
-      display: 'flex',
-      justifyContent: 'flex-end',
-      background: '#0D646A',
-      padding: '0.3rem 2rem 5rem 0',
-      boxSizing: 'border-box',
-    }}
-  >
-    <h1 style={{ display: 'flex', justifyContent: 'center' }}>
+  <header className={styles.header}>
+    <h1 className={styles.siteHeader}>
       <Link
         to="/"
         style={{
